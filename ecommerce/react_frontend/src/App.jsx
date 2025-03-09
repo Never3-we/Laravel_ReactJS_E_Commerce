2b1a95bb1affc9a37a9b43e934a336ad444db6a1
khemerak <pavkhemerak19@gmail.com>
2025-03-10 02:13:32 +0700

    Shop page

--- ecommerce/react_frontend/src/App.jsx
+++ ecommerce/react_frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          
+          <Route path='/shop' element={<h1>Shop</h1>} />
         </Routes>
         <Footer />
       </Router>
